a3ce37369c252e5800d37b4cae9cc0e0563fa9ac
USUARIO <USUARIO@LAPTOP-BUKF8MRB>
2023-10-04 23:35:52 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio_07_SMN_7_ACT_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Ingrese un número límite: ");
+int limite = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Divisores de Omar Orlandini en el rango del 1 al {limite}:");
+
+for (int i = 1; i <= limite; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine($"Divisor de Omar Orlandini {i / 3}: {i}");
+    }
+}
+Console.WriteLine("Limite alcanzado: " + limite);
